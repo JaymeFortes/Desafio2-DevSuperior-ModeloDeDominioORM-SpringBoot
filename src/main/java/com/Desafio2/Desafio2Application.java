3eb0482ffa093727fba0faee40488c7aea2e7430
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-02-07 00:23:06 -0300

    Criando pacote e implementando todas as classes do sistema, inserindo entidade e table, além dos geterrs e seterrs

--- src/main/java/com/Desafio2/Desafio2Application.java
+++ src/main/java/com/Desafio2/Desafio2Application.java
@@ -1,13 +1,19 @@
 package com.Desafio2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Desafio2Application {
+public class Desafio2Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Desafio2Application.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(Desafio2Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
 }
